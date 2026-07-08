739434cb7965c980d5940d04f63207edd759a1de
도병찬 <bcdo0316@naver.com>
2026-07-09 00:42:52 +0900

    modify

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -80,14 +80,13 @@ const StyledSky = styled.div`
 
   span {
     position: absolute;
-    width: 3px;
-    height: 3px;
+    width: 7px;
+    height: 7px;
     border-radius: 9999px;
     background-color: rgba(255, 255, 255, 0.85);
     box-shadow: 0 0 2px rgba(255, 255, 255, 0.70);
     opacity: 0;
-    transform: translate3d(0, 0, 0) rotate(-35deg);
-    animation-name: shootingStar;
+    transform: translate3d(0, 0, 0) rotate(35deg);
     animation-timing-function: ease-out;
     animation-iteration-count: infinite;
 
@@ -95,12 +94,12 @@ const StyledSky = styled.div`
       content: "";
       position: absolute;
       top: 50%;
-      right: 2px;
+      left: 4px;
       width: 96px;
       height: 1px;
       transform: translateY(-50%);
       background: linear-gradient(
-        90deg,
+        270deg,
         transparent,
         rgba(255, 255, 255, 0.50)
       );
@@ -109,56 +108,137 @@ const StyledSky = styled.div`
     &:nth-of-type(1) {
       top: 12%;
       left: 86%;
-      animation-duration: 2.4s;
+      animation-name: shootingStarFirst;
+      animation-duration: 23s;
       animation-delay: 1.2s;
     }
 
     &:nth-of-type(2) {
       top: 26%;
       left: 104%;
-      width: 2px;
-      height: 2px;
-      animation-duration: 2.9s;
+      width: 6px;
+      height: 6px;
+      animation-name: shootingStarSecond;
+      animation-duration: 29s;
       animation-delay: 7.4s;
     }
 
     &:nth-of-type(3) {
       top: 44%;
       left: 92%;
-      width: 4px;
-      height: 4px;
-      animation-duration: 2.2s;
+      width: 8px;
+      height: 8px;
+      animation-name: shootingStarThird;
+      animation-duration: 37s;
       animation-delay: 13.6s;
     }
 
     &:nth-of-type(4) {
       top: 8%;
       left: 64%;
-      width: 2px;
-      height: 2px;
-      animation-duration: 2.6s;
+      width: 6px;
+      height: 6px;
+      animation-name: shootingStarFourth;
+      animation-duration: 43s;
       animation-delay: 18.8s;
     }
 
     &:nth-of-type(5) {
       top: 62%;
       left: 98%;
-      animation-duration: 2.7s;
+      animation-name: shootingStarFifth;
+      animation-duration: 49s;
       animation-delay: 24.2s;
     }
   }
 
-  @keyframes shootingStar {
+  @keyframes shootingStarFirst {
     0% {
       opacity: 0;
-      transform: translate3d(0, 0, 0) rotate(-35deg);
+      transform: translate3d(0, 0, 0) rotate(35deg);
     }
-    12% {
+    1% {
       opacity: 0.85;
+    }
+    10% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
     }
     100% {
       opacity: 0;
-      transform: translate3d(-54vw, 38vh, 0) rotate(-35deg);
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+  }
+
+  @keyframes shootingStarSecond {
+    0% {
+      opacity: 0;
+      transform: translate3d(0, 0, 0) rotate(35deg);
+    }
+    1% {
+      opacity: 0.85;
+    }
+    9% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+    100% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+  }
+
+  @keyframes shootingStarThird {
+    0% {
+      opacity: 0;
+      transform: translate3d(0, 0, 0) rotate(35deg);
+    }
+    1% {
+      opacity: 0.85;
+    }
+    7.6% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+    100% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+  }
+
+  @keyframes shootingStarFourth {
+    0% {
+      opacity: 0;
+      transform: translate3d(0, 0, 0) rotate(35deg);
+    }
+    1% {
+      opacity: 0.85;
+    }
+    6% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+    100% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+  }
+
+  @keyframes shootingStarFifth {
+    0% {
+      opacity: 0;
+      transform: translate3d(0, 0, 0) rotate(35deg);
+    }
+    1% {
+      opacity: 0.85;
+    }
+    5.8% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
+    }
+    100% {
+      opacity: 0;
+      transform: translate3d(-54vw, 38vh, 0) rotate(35deg);
     }
   }
 `
